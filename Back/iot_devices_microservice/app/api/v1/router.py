1c45d9aab3918e6c7f8011429f835bc8876ce305
Richardguerr <oscarguerr0205@gmail.com>
2025-10-21 13:47:11 -0500

    feat: new fatures

--- Back/iot_devices_microservice/app/api/v1/router.py
+++ Back/iot_devices_microservice/app/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import sensors, sensor_nodes, iot_gateways, mine_zones
+from app.api.v1.endpoints import sensors, sensor_nodes, iot_gateways, mine_zones, users
 
 api_router = APIRouter()
 
@@ -7,3 +7,4 @@ api_router.include_router(sensors.router, prefix="/sensors", tags=["Sensores"])
 api_router.include_router(sensor_nodes.router, prefix="/sensor-nodes", tags=["Nodos Sensores"])
 api_router.include_router(iot_gateways.router, prefix="/iot-gateways", tags=["IoT Gateways"])
 api_router.include_router(mine_zones.router, prefix="/mine-zones", tags=["Mines Zones"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
